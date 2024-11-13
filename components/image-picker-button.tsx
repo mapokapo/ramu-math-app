b2a13d6a55ce200dd41677b147732d52bf03cd2d
mapokapo <leopetrovic11@gmail.com>
2024-11-13 11:11:28 +0100

    refactor: make image picker button better

--- components/image-picker-button.tsx
+++ components/image-picker-button.tsx
@@ -1,18 +1,29 @@
-import { Image, TouchableOpacity } from "react-native";
+import {
+  Image,
+  StyleProp,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ViewStyle,
+} from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { toast } from "burnt";
+import ThemedView from "./themed-view";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useTheme } from "../lib/hooks/theme";
 
-type Props = {
+type Props = Omit<TouchableOpacityProps, "style"> & {
+  style?: StyleProp<ViewStyle>;
+} & {
   value: string | null;
   onImagePicked: (uri: string) => void;
   disabled?: boolean;
 };
 
-export default function ImagePickerButton({
-  value,
-  onImagePicked,
-  disabled = false,
-}: Props) {
+export default function ImagePickerButton(props: Props) {
+  const { value, onImagePicked, disabled } = props;
+
+  const theme = useTheme();
+
   const handlePickImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -38,16 +49,50 @@ export default function ImagePickerButton({
   };
   return (
     <TouchableOpacity
-      onPress={handlePickImage}
-      disabled={disabled}>
-      <Image
-        source={{ uri: value ?? undefined }}
-        style={{
+      {...props}
+      style={[
+        {
+          backgroundColor: props.disabled
+            ? theme.colors.muted
+            : theme.colors.primary,
+          alignItems: "center",
+          borderRadius: 9999,
+          aspectRatio: 1,
+          overflow: "hidden",
           width: 100,
           height: 100,
-          borderRadius: 50,
-        }}
-      />
+        },
+        props.style,
+      ]}
+      onPress={handlePickImage}
+      disabled={disabled}>
+      {value !== null ? (
+        <Image
+          source={{ uri: value }}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      ) : (
+        <ThemedView
+          style={{
+            borderRadius: 9999,
+            padding: 16,
+            backgroundColor: theme.colors.primary,
+            aspectRatio: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            width: 100,
+            height: 100,
+          }}>
+          <Ionicons
+            name="camera"
+            size={48}
+            color={theme.colors.background}
+          />
+        </ThemedView>
+      )}
     </TouchableOpacity>
   );
 }
